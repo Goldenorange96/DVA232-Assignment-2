125b626d629ff2a2eb29cd8a858dd8efcc01be43
GoldenOrange <goldenorange1996@gmail.com>
2016-12-09 16:44:58 +0100

    Second Commit

--- app/src/main/java/com/example/goldenorange/dva232_assignment_2/MainActivity.java
+++ app/src/main/java/com/example/goldenorange/dva232_assignment_2/MainActivity.java
@@ -1,30 +1,34 @@
 package com.example.goldenorange.dva232_assignment_2;
 
 import android.content.Intent;
+import android.content.ServiceConnection;
 import android.content.pm.PackageManager;
 import android.graphics.Bitmap;
 import android.graphics.Color;
+import android.graphics.drawable.BitmapDrawable;
+import android.graphics.drawable.Drawable;
 import android.os.Environment;
 import android.provider.MediaStore;
+import android.support.v4.content.ContextCompat;
 import android.support.v4.content.FileProvider;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.widget.ImageView;
 import android.widget.Button;
 import android.view.View;
+import android.view.View.OnClickListener;
 import android.net.Uri;
+import android.widget.Toast;
+import android.content.Context;
 
-import java.io.File;
-import java.text.SimpleDateFormat;
-import java.util.Date;
-import java.util.Locale;
 
-public class MainActivity extends AppCompatActivity
-{
+public class MainActivity extends AppCompatActivity implements OnClickListener{
     String currentPhotoPath = null;
     static final int REQUEST_IMAGE_CAPTURE = 1;
-    public ImageView photoView = null;
-    public Bitmap photo = null;
+    private ImageView photoView;
+    private Bitmap photo;
+    private Drawable testImage;
 
 
     @Override
@@ -34,73 +38,59 @@ public class MainActivity extends AppCompatActivity
 
         Button greyScaleButton = (Button) findViewById(R.id.Greyscale);
         Button negateButton = (Button) findViewById(R.id.Negate);
-
-        greyScaleButton.setVisibility(View.INVISIBLE);
-        negateButton.setVisibility(View.INVISIBLE);
-
+        negateButton.setOnClickListener(this);
+        greyScaleButton.setOnClickListener(this);
 
         Button cameraButton = (Button) findViewById(R.id.cameraButton);
         photoView = (ImageView) findViewById(R.id.capturedImage);
 
-
         //Check if the device has a camera or not.
-        if(!hasCamera())
-        {
+        if (!hasCamera()) {
             cameraButton.setEnabled(false);
         }
     }
 
-
-
-    private boolean hasCamera()
+    public void onClick(View v)
     {
+        photoView = (ImageView) findViewById(R.id.capturedImage);
+        switch (v.getId())
+        {
+            case R.id.Negate:
+                photo = setNegate(photo);
+                photoView.setImageBitmap(photo);
+
+                break;
+
+            case R.id.Greyscale:
+                photo = setGreyscale(photo);
+                photoView.setImageBitmap(photo);
+                break;
+
+            default:
+                break;
+
+        }
+    }
+
+    private boolean hasCamera() {
         return getPackageManager().hasSystemFeature(PackageManager.FEATURE_CAMERA_ANY);
     }
 
-    public void setGreyscale(View view)
+    public Bitmap setNegate(Bitmap original)
     {
         //Create a copy of the bitmap image with it's width and height. The config describes how the bitmap has stored its pixels.
-        Bitmap resultImage = Bitmap.createBitmap(photo.getWidth(), photo.getHeight(), photo.getConfig());
-        double A = 0, R = 0, G = 0, B = 0;
+        Bitmap resultImage = Bitmap.createBitmap(original.getWidth(), original.getHeight(), original.getConfig());
+        int A, R, G, B;
         int height = resultImage.getHeight();
         int width = resultImage.getWidth();
-        int pixelcolour = 0;
+        int pixelcolour;
 
         //Loop over each pixel in the bitmap.
 
-        for(int y = 0; y < height; ++y)
-        {
-            for(int x = 0; x < width; ++x)
-            {
+        for (int y = 0; y < height; y++) {
+            for (int x = 0; x < width; x++) {
                 //Get the pixels colours.
-                pixelcolour = resultImage.getPixel(x ,y);
-                A = (Color.alpha(pixelcolour));
-                R = (0.2126 * Color.red(pixelcolour));
-                G = (0.7152 * Color.green(pixelcolour));
-                B = (0.0722 * Color.blue(pixelcolour));
-                resultImage.setPixel(x, y, Color.argb((int)A, (int)R, (int)G, (int)B));
-            }
-        }
-        photoView.setImageBitmap(resultImage);
-    }
-
-    public void setNegate(View view)
-    {
-        //Create a copy of the bitmap image with it's width and height. The config describes how the bitmap has stored its pixels.
-        Bitmap resultImage = Bitmap.createBitmap(photo.getWidth(), photo.getHeight(), photo.getConfig());
-        int A = 0, R = 0, G = 0, B = 0;
-        int height = resultImage.getHeight();
-        int width = resultImage.getWidth();
-        int pixelcolour = 0;
-
-        //Loop over each pixel in the bitmap.
-
-        for(int y = 0; y < height; ++y)
-        {
-            for(int x = 0; x < width; ++x)
-            {
-                //Get the pixels colours.
-                pixelcolour = resultImage.getPixel(x ,y);
+                pixelcolour = original.getPixel(x, y);
                 A = Color.alpha(pixelcolour);
                 R = 255 - Color.red(pixelcolour);
                 G = 255 - Color.green(pixelcolour);
@@ -108,51 +98,49 @@ public class MainActivity extends AppCompatActivity
                 resultImage.setPixel(x, y, Color.argb(A, R, G, B));
             }
         }
-        photo = resultImage;
-        photoView.setImageBitmap(resultImage);
+        return resultImage;
     }
 
-    public void launchCamera(View view)
+    public Bitmap setGreyscale(Bitmap original)
     {
-        Button greyScaleButton = (Button) findViewById(R.id.Greyscale);
-        Button negateButton = (Button) findViewById(R.id.Negate);
+        //Create a copy of the bitmap image with it's width and height. The config describes how the bitmap has stored its pixels.
+        Bitmap resultImage = Bitmap.createBitmap(original.getWidth(), original.getHeight(), original.getConfig());
+        int A, R, G, B, grey;
+        int height = resultImage.getHeight();
+        int width = resultImage.getWidth();
+        int pixelcolour;
+
+        //Loop over each pixel in the bitmap.
+
+        for (int y = 0; y < height; y++) {
+            for (int x = 0; x < width; x++) {
+                //Get the pixels colours.
+                pixelcolour = original.getPixel(x, y);
+                A = Color.alpha(pixelcolour);
+                R = Color.red(pixelcolour);
+                G = Color.green(pixelcolour);
+                B = Color.blue(pixelcolour);
+                grey = (R + G + B)/3;
+                resultImage.setPixel(x, y, Color.argb(A, grey, grey, grey));
+            }
+        }
+        return resultImage;
+    }
+
+    public void launchCamera(View view) {
         Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
 
-       /* File photoFile = null;
-
-        photoFile = createImageFile();
-
-        if(photoFile != null)
-        {
-            Uri photoURI = FileProvider.getUriForFile(this, "com.example.android.fileprovider", photoFile);
-            intent.putExtra(MediaStore.EXTRA_OUTPUT, photoURI);
-        }*/
-
         startActivityForResult(intent, REQUEST_IMAGE_CAPTURE);
-        greyScaleButton.setVisibility(View.VISIBLE);
-        negateButton.setVisibility(View.VISIBLE);
     }
 
     //Save the returned image and set the imageview to it.
     @Override
-    protected void onActivityResult(int requestCode, int resultCode, Intent data)
-    {
-        if(requestCode == REQUEST_IMAGE_CAPTURE && resultCode == RESULT_OK)
-        {
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if (requestCode == REQUEST_IMAGE_CAPTURE && resultCode == RESULT_OK) {
             Bundle extras = data.getExtras();
             photo = (Bitmap) extras.get("data");
             photoView.setImageBitmap(photo);
         }
     }
-
-    /*private File createImageFile()
-    {
-        String timeStamp = new SimpleDateFormat("yyyyMMdd_HHmmss", Locale.US).format(new Date());
-        String imageFileName = "Photo_Camera" + timeStamp + "_";
-        File storageDir = getExternalFilesDir(Environment.DIRECTORY_PICTURES);
-        File image = File.createTempFile(imageFileName, ".jpg", storageDir);
-
-        currentPhotoPath = image.getAbsolutePath();
-        */
-
+}
 
