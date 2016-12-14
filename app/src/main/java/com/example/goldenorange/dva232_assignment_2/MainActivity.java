24dd64a8de8c830491a50ec57a1a7563e9b73618
GoldenOrange <goldenorange1996@gmail.com>
2016-12-14 14:23:05 +0100

    Final commit, Labb finished and approved.

--- app/src/main/java/com/example/goldenorange/dva232_assignment_2/MainActivity.java
+++ app/src/main/java/com/example/goldenorange/dva232_assignment_2/MainActivity.java
@@ -1,6 +1,5 @@
 package com.example.goldenorange.dva232_assignment_2;
 
-import android.content.Context;
 import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.graphics.Bitmap;
@@ -29,10 +28,14 @@ import java.util.Locale;
 
 public class MainActivity extends AppCompatActivity implements OnClickListener{
     static final int REQUEST_IMAGE_CAPTURE = 1;
+    public int greyscaleSet = 0, negateSet = 0;
     private ImageView photoView;
-    private Bitmap photo;
+    private Bitmap photo, editedPhoto;
     public final String DIR_TAG = "directoryLogTag";
     private static final int APPDEF_REQUEST_WRITE_FILE = 1;
+    Button greyScaleButton;
+    Button negateButton;
+    Button saveButton;
 
 
     @Override
@@ -40,12 +43,15 @@ public class MainActivity extends AppCompatActivity implements OnClickListener{
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        Button greyScaleButton = (Button) findViewById(R.id.Greyscale);
-        Button negateButton = (Button) findViewById(R.id.Negate);
-        Button saveButton = (Button) findViewById(R.id.saveButton);
+        greyScaleButton = (Button) findViewById(R.id.Greyscale);
+        negateButton = (Button) findViewById(R.id.Negate);
+        saveButton = (Button) findViewById(R.id.saveButton);
         negateButton.setOnClickListener(this);
         greyScaleButton.setOnClickListener(this);
         saveButton.setOnClickListener(this);
+        negateButton.setEnabled(false);
+        greyScaleButton.setEnabled(false);
+        saveButton.setEnabled(false);
 
         Button cameraButton = (Button) findViewById(R.id.cameraButton);
         photoView = (ImageView) findViewById(R.id.capturedImage);
@@ -62,14 +68,13 @@ public class MainActivity extends AppCompatActivity implements OnClickListener{
         switch (v.getId())
         {
             case R.id.Negate:
-                photo = setNegate(photo);
-                photoView.setImageBitmap(photo);
-
+                editedPhoto = setNegate(photo);
+                photoView.setImageBitmap(editedPhoto);
                 break;
 
             case R.id.Greyscale:
-                photo = setGreyscale(photo);
-                photoView.setImageBitmap(photo);
+                editedPhoto = setGreyscale(photo);
+                photoView.setImageBitmap(editedPhoto);
                 break;
 
             case R.id.saveButton:
@@ -89,6 +94,7 @@ public class MainActivity extends AppCompatActivity implements OnClickListener{
         }
     }
 
+    //Check if the device has any camera.
     private boolean hasCamera() {
         return getPackageManager().hasSystemFeature(PackageManager.FEATURE_CAMERA_ANY);
     }
@@ -104,18 +110,28 @@ public class MainActivity extends AppCompatActivity implements OnClickListener{
 
         //Loop over each pixel in the bitmap.
 
-        for (int y = 0; y < height; y++) {
-            for (int x = 0; x < width; x++) {
-                //Get the pixels colours.
-                pixelcolour = original.getPixel(x, y);
-                A = Color.alpha(pixelcolour);
-                R = 255 - Color.red(pixelcolour);
-                G = 255 - Color.green(pixelcolour);
-                B = 255 - Color.blue(pixelcolour);
-                resultImage.setPixel(x, y, Color.argb(A, R, G, B));
-            }
+        if(negateSet == 1)
+        {
+            negateSet = 0;
+            return photo;
         }
-        return resultImage;
+
+        else
+        {
+            for (int y = 0; y < height; y++) {
+                for (int x = 0; x < width; x++) {
+                    //Get the pixels colours.
+                    pixelcolour = original.getPixel(x, y);
+                    A = Color.alpha(pixelcolour);
+                    R = 255 - Color.red(pixelcolour);
+                    G = 255 - Color.green(pixelcolour);
+                    B = 255 - Color.blue(pixelcolour);
+                    resultImage.setPixel(x, y, Color.argb(A, R, G, B));
+                }
+            }
+            negateSet = 1;
+            return resultImage;
+        }
     }
 
     public Bitmap setGreyscale(Bitmap original)
@@ -129,38 +145,33 @@ public class MainActivity extends AppCompatActivity implements OnClickListener{
 
         //Loop over each pixel in the bitmap.
 
-        for (int y = 0; y < height; y++) {
-            for (int x = 0; x < width; x++) {
-                //Get the pixels colours.
-                pixelcolour = original.getPixel(x, y);
-                A = Color.alpha(pixelcolour);
-                R = Color.red(pixelcolour);
-                G = Color.green(pixelcolour);
-                B = Color.blue(pixelcolour);
-                grey = (R + G + B)/3;
-                resultImage.setPixel(x, y, Color.argb(A, grey, grey, grey));
+        if(greyscaleSet == 1)
+        {
+            resultImage = photo;
+            greyscaleSet = 0;
+            return resultImage;
+        }
+        else
+        {
+            for (int y = 0; y < height; y++) {
+                for (int x = 0; x < width; x++) {
+                    //Get the pixels colours.
+                    pixelcolour = original.getPixel(x, y);
+                    A = Color.alpha(pixelcolour);
+                    R = Color.red(pixelcolour);
+                    G = Color.green(pixelcolour);
+                    B = Color.blue(pixelcolour);
+                    grey = (R + G + B)/3;
+                    resultImage.setPixel(x, y, Color.argb(A, grey, grey, grey));
+                }
             }
+            greyscaleSet = 1;
+            return resultImage;
         }
-        return resultImage;
+
     }
-
-
-    public static Camera openCamera(View view)
-    {
-        Camera camera = null;
-        try
-        {
-            //Is decrepted but application is made on earlier API.
-            camera = Camera.open();
-        }
-        catch (Exception e)
-        {
-            Log.e("Hello world",e.getMessage());
-        }
-        return camera;
-    }
-
-    /*public void launchCamera(View view) {
+    //Launch the device default camera app via an intent and create a temporary file for the image.
+    public void launchCamera(View view) {
         Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
 
         startActivityForResult(intent, REQUEST_IMAGE_CAPTURE);
@@ -173,20 +184,25 @@ public class MainActivity extends AppCompatActivity implements OnClickListener{
             Bundle extras = data.getExtras();
             photo = (Bitmap) extras.get("data");
             photoView.setImageBitmap(photo);
+            greyScaleButton.setEnabled(true);
+            negateButton.setEnabled(true);
+            saveButton.setEnabled(true);
         }
-    }*/
+    }
 
     //Check if permission is granted for write_to_external - For android with the new run-time permission model.
 
     private void checkPermission()
     {
-        if((ContextCompat.checkSelfPermission(this, Manifest.permission.WRITE_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED));
+        if((ContextCompat.checkSelfPermission(this, Manifest.permission.WRITE_EXTERNAL_STORAGE) == PackageManager.PERMISSION_GRANTED))
         {
             Toast toast = Toast.makeText(getApplicationContext(), "Checking Permissions", Toast.LENGTH_LONG);
             toast.show();
             ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.WRITE_EXTERNAL_STORAGE}, APPDEF_REQUEST_WRITE_FILE);
         }
     }
+
+    //If permission is granted then do what the operations.
 
     @Override
     public void onRequestPermissionsResult(int requestCode, String permissions[], int[] grantResults)
@@ -199,8 +215,8 @@ public class MainActivity extends AppCompatActivity implements OnClickListener{
                 {
                     //Create a unique filename using a timestamp.
                     String filename = new SimpleDateFormat("yyyy.MM.dd'T'HH:mm:ss", Locale.ENGLISH).format(new Date());
-                    String fileName = filename + ".jpg";
-                    File newFile = new File(getPhotoStorageDir(), fileName);
+                    filename = filename + ".jpg";
+                    File newFile = new File(getPhotoStorageDir(), filename);
 
                     try
                     {
@@ -208,12 +224,13 @@ public class MainActivity extends AppCompatActivity implements OnClickListener{
                         FileOutputStream f = new FileOutputStream(newFile);
                         if(photo.compress(Bitmap.CompressFormat.JPEG, 100, f))
                         {
-                            Toast toast = Toast.makeText(getApplicationContext(), "Compression completed", Toast.LENGTH_LONG);
+                            Toast toast = Toast.makeText(getApplicationContext(), "Picture saved", Toast.LENGTH_LONG);
                             toast.show();
+                            f.flush();
                         }
                         else
                         {
-                            Toast toast = Toast.makeText(getApplicationContext(), "Compressition Failed", Toast.LENGTH_LONG);
+                            Toast toast = Toast.makeText(getApplicationContext(), "Picture was not saved", Toast.LENGTH_LONG);
                             toast.show();
                         }
                     }
@@ -255,5 +272,25 @@ public class MainActivity extends AppCompatActivity implements OnClickListener{
         return photoDir;
     }
 
+
+    //Create a temporary file so that thee full sized image can be displayed.
+    private File createTemporaryFile(String name, String suff) throws Exception
+    {
+        //Create an internal directory with the name temp.
+        File tempDir = Environment.getExternalStorageDirectory();
+        tempDir = new File(tempDir.getAbsolutePath()+"/.temp/");
+        if(!tempDir.exists())
+        {
+            if(tempDir.mkdir())
+            {
+                Log.e(DIR_TAG, "Temporary directory has been created");
+            }
+            else
+            {
+                Log.e(DIR_TAG, "Temporary directory has not been created");
+            }
+        }
+        return File.createTempFile(name, suff, tempDir);
+    }
 }
 
